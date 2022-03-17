4fa8772ecb23ddc41c54ccb70ed4231ddf832796
KieArm <87212808+KieArm@users.noreply.github.com>
2022-03-17 15:38:16 +0800

    17/03/2022
    Updated about section.
    Updated projects section.
    Added new project: ds-info.
    Added ds-info assets.
    Removed old projects: Figma.
    Added titles to work & project sections.
    Added new animation to hero section.
    Prettify all code.

--- js/main.js
+++ js/main.js
@@ -1,61 +1,61 @@
 // AOS Initialize
 AOS.init({
-    disable: 'mobile'
+  disable: "mobile",
 });
 // Popper Tooltip
-const button = document.querySelector('#button');
-const tooltip = document.querySelector('#tooltip');
+const button = document.querySelector("#button");
+const tooltip = document.querySelector("#tooltip");
 
 const popperInstance = Popper.createPopper(button, tooltip, {
-    placement: 'bottom',
-    modifiers: [
-        {
-            name: 'offset',
-            options: {
-                offset: [0, 8],
-            },
-        },
-    ],
+  placement: "bottom",
+  modifiers: [
+    {
+      name: "offset",
+      options: {
+        offset: [0, 8],
+      },
+    },
+  ],
 });
 
 function show() {
-    tooltip.setAttribute('data-show', '');
-    popperInstance.setOptions({
-        modifiers: [{ name: 'eventListeners', enabled: true }],
-    });
-    popperInstance.update();
+  tooltip.setAttribute("data-show", "");
+  popperInstance.setOptions({
+    modifiers: [{ name: "eventListeners", enabled: true }],
+  });
+  popperInstance.update();
 }
 
 function hide() {
-    tooltip.removeAttribute('data-show');
-    popperInstance.setOptions({
-        modifiers: [{ name: 'eventListeners', enabled: false }],
-    });
+  tooltip.removeAttribute("data-show");
+  popperInstance.setOptions({
+    modifiers: [{ name: "eventListeners", enabled: false }],
+  });
 }
 
-const showEvents = ['mouseenter', 'focus'];
-const hideEvents = ['mouseleave', 'blur'];
+const showEvents = ["mouseenter", "focus"];
+const hideEvents = ["mouseleave", "blur"];
 
-showEvents.forEach(event => {
-    button.addEventListener(event, show);
+showEvents.forEach((event) => {
+  button.addEventListener(event, show);
 });
 
-hideEvents.forEach(event => {
-    button.addEventListener(event, hide);
+hideEvents.forEach((event) => {
+  button.addEventListener(event, hide);
 });
 // Navbar Update on Scroll Position
-const spyScrolling = ( ) => {
-    const sections = document.querySelectorAll( '.scroll-bg' );
-  
-    window.onscroll = ( ) => {
-      const scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
-  
-      for ( let s in sections )
-        if ( sections.hasOwnProperty( s ) && sections[ s ].offsetTop <= scrollPos ) {
-          const id = sections[ s ].id;
-          document.querySelector( '.active' ).classList.remove( 'active' );
-          document.querySelector( `a[href*=${ id }]` ).classList.add( 'active' );
-        }
-    } 
-}
-spyScrolling( );
+const spyScrolling = () => {
+  const sections = document.querySelectorAll(".scroll-bg");
+
+  window.onscroll = () => {
+    const scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
+
+    for (let s in sections)
+      if (sections.hasOwnProperty(s) && sections[s].offsetTop <= scrollPos) {
+        const id = sections[s].id;
+        document.querySelector(".active").classList.remove("active");
+        document.querySelector(`a[href*=${id}]`).classList.add("active");
+      }
+  };
+};
+spyScrolling();
